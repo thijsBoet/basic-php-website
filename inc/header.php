6853086bfdf16403c7934271f7178d16027bb1a8
Matthijs Boet <m.boet@chello.nl>
2021-07-03 01:40:24 +0200

    finished media library project

--- inc/header.php
+++ inc/header.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-    <title><?php echo "My Personal Media Library"; ?></title>
+    <title><?php echo $pageTitle; ?></title>
     <link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
 
@@ -14,10 +14,15 @@
             <h1 class="branding-title"><a href="./">Personal Media Library</a></h1>
 
             <ul class="nav">
-                <li class="books"><a href="catalog.php?cat=books">Books</a></li>
-                <li class="movies"><a href="catalog.php?cat=movies">Movies</a></li>
-                <li class="music"><a href="catalog.php?cat=music">Music</a></li>
-                <li class="suggest"><a href="suggest.php">Suggest</a></li>
+                <li class="books <?php if($section == "books") { echo " on";} ?>"><a
+                        href="catalog.php?cat=books">Books</a>
+                </li>
+                <li class="movies <?php if($section == "movies") { echo " on";} ?>"><a
+                        href="catalog.php?cat=movies">Movies</a></li>
+                <li class="music <?php if($section == "music") { echo " on";} ?>"><a
+                        href="catalog.php?cat=music">Music</a></li>
+                <li class="suggest <?php if($section == "suggest") { echo " on";} ?>"><a href="suggest.php">Suggest</a>
+                </li>
             </ul>
 
         </div>
